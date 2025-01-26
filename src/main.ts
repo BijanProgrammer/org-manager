2a4e0f9a3745721f1b09d0d1dd2936f3f8d79e2f
BijanProgrammer <bijaneisapour@gmail.com>
2025-01-26 22:38:57 +0330

    [START] react 2025.02 bootcamp

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import "dotenv/config";
 import "./configs/bootcamp-2025-02-react.config.js";
 
+import { resetMembers } from "./actions/member.actions.js";
+import { createRepoForAllMembers } from "./actions/repo.actions.js";
+import { grantAccessForAllMembers } from "./actions/access.actions.js";
+import { addRulesetToAllRepos } from "./actions/ruleset.actions.js";
+
 async function main(): Promise<void> {
   try {
+    await resetMembers();
+    await createRepoForAllMembers();
+    await addRulesetToAllRepos();
+    await grantAccessForAllMembers();
   } catch (err) {
     console.log(err);
   }
